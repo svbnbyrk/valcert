da0a6fed432659c5a836f899e498ba2a41e3e746
Sevban Bayrak <sevban.bayrak@sabancidx.com>
2021-07-28 16:12:54 +0300

    Added CAB forums baseline requirements

--- main.go
+++ main.go
@@ -30,15 +30,23 @@ func main() {
 		fmt.Printf("%s match with certificate\n", hn)
 
 		expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
+		start := conn.ConnectionState().PeerCertificates[0].NotBefore
 		fmt.Printf("Issuer: %s\n", conn.ConnectionState().PeerCertificates[0].Issuer)
 
 		today := time.Now()
+		d := expiry.Sub(today).Hours() / 24
+		//CAB forum’s baseline requirements.
+		sed := time.Date(2020, time.September, 1, 0, 0, 0, 0, time.Now().UTC().Location())
 
 		if expiry.After(today) {
-			d := expiry.Sub(today).Hours() / 24
-
 			fmt.Printf("Expiry: %v\nYour certificate expires in %.1f days.", expiry.Format(time.RFC850), d)
-			if d > 7 {
+			if start.After(sed) {
+				if d > 398 {		
+					fmt.Printf("Certificates issued after September 1, 2020 should have validity periods no longer than 398 days.")			
+					fmt.Printf(" Please contact your Certificate Authority (CA)\n")					
+				}
+			}
+			if d < 14 {
 				fmt.Printf(" Please contact your Certificate Authority (CA)\n")
 			}
 		} else {
