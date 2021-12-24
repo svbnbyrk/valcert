292d98525ff31cee553afbba193c83b3a243c18e
Sevban Bayrak <svbnbyrk@Sevbans-MacBook-Pro.local>
2021-12-24 22:07:25 +0300

    Fix lint warning

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	r, _ := regexp.MatchString(`[^\s.]+(?:\.[a-z]+)*(?::\d+)`, args[0])
+	r, _ := regexp.MatchString(`[^\s.]+(?:\.[a-z]+)*?::\d+`, args[0])
 	if !r || len(args) > 1 || args[0] == "-help" {
 		fmt.Println("Usage: valcert example.com:443")
 	} else {
@@ -28,39 +28,39 @@ func main() {
 		if err != nil {
 			panic("Hostname doesn't match with certificate: " + err.Error())
 		}
-		
+
 		fmt.Printf("%s match with certificate\n", hn)
 		fmt.Println("Certificates:")
 		for i := 0; i < len(conn.ConnectionState().PeerCertificates); i++ {
-			cert := conn.ConnectionState().PeerCertificates[i] 
-			revoke, ok := revoke.VerifyCertificate(cert)
+			cert := conn.ConnectionState().PeerCertificates[i]
+			isRevoke, ok := revoke.VerifyCertificate(cert)
 
-			if(revoke == true){
+			if isRevoke == true {
 				fmt.Println("Certificate is revoked.")
-			} 
-			if(ok == false){
+			}
+			if ok == false {
 				fmt.Println("Certificate is not valid.")
 			}
 			expiry := cert.NotAfter
 			start := cert.NotBefore
-			issuer:= cert.Issuer
+			issuer := cert.Issuer
 			subject := cert.Subject
 			fmt.Printf("\nIssuer: %s", issuer)
 			fmt.Printf("\nSubject: %s", subject)
-			if(issuer.CommonName == subject.CommonName){
-				fmt.Println("Certificate is Selfsigned. Please contact your Certificate Authority (CA)")
+			if issuer.CommonName == subject.CommonName {
+				fmt.Println("Certificate is Self signed. Please contact your Certificate Authority (CA)")
 			}
 			today := time.Now()
 			d := expiry.Sub(today).Hours() / 24
 			//CAB forum’s baseline requirements.
 			sed := time.Date(2020, time.September, 1, 0, 0, 0, 0, time.Now().UTC().Location())
-	
+
 			if expiry.After(today) {
 				fmt.Printf("\nExpiry: %v\nYour certificate expires in %.1f days.", expiry.Format(time.RFC850), d)
 				if start.After(sed) {
-					if d > 398 {		
-						fmt.Printf("\nCertificates issued after September 1, 2020 should have validity periods no longer than 398 days.")			
-						fmt.Printf("Please contact your Certificate Authority (CA)")					
+					if d > 398 {
+						fmt.Printf("\nCertificates issued after September 1, 2020 should have validity periods no longer than 398 days.")
+						fmt.Printf("Please contact your Certificate Authority (CA)")
 					}
 				}
 				if d < 14 {
